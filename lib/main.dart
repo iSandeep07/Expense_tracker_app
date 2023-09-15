ec72383f4ef8f1273b3e53f2861845471995848d
isandy8 <113350985+isandy8@users.noreply.github.com>
2023-09-15 21:52:56 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
+//import 'package:flutter/services.dart';
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed( brightness : Brightness.dark , seedColor: Color.fromARGB(255, 5, 99, 125));
+
+void main() {
+ // WidgetsFlutterBinding.ensureInitialized();
+
+ // SystemChrome.setPreferredOrientations([
+  //  DeviceOrientation.portraitUp,
+ //]).then((fn){
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(useMaterial3: true,
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer
+          ),
+          ),
+        ),
+
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(useMaterial3: true , colorScheme: kColorScheme ,
+            appBarTheme: AppBarTheme().copyWith(
+                backgroundColor: kColorScheme.onPrimaryContainer,
+                foregroundColor: kColorScheme.primaryContainer
+            ),
+            cardTheme: CardTheme().copyWith(
+                color: kColorScheme.secondaryContainer,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                )
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer
+            ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(fontWeight: FontWeight.bold, color: kColorScheme.onSecondaryContainer, fontSize: 16)
+            )
+        ),
+
+        themeMode: ThemeMode.system,
+        home: Expenses(),
+      ),
+    );
+  }
+
